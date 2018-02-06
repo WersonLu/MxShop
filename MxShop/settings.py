bddd16bf58aba58f89996fbf98cd85f05ee25d50
WersonLu <wersonliu@gmail.com>
2018-02-06 14:08:24 +0800

    项目初始化 用户,订单,商品模型建立

--- MxShop/settings.py
+++ MxShop/settings.py
@@ -40,6 +40,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'users',
+    'goods',
+    'DjangoUeditor',
+    'trade',
+    'user_operation',
+
 ]
 
 MIDDLEWARE = [
